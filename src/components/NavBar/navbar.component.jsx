988f0d288bd4494781424d3a8fccd1bc4e592128
Unknown <daniromeromunoz16@gmail.com>
2019-02-06 00:03:17 +0100

    NavBar Designed

--- src/components/NavBar/navbar.component.jsx
+++ src/components/NavBar/navbar.component.jsx
@@ -1,18 +1,26 @@
 import React, { Component, Fragment } from 'react';
 import styles from './navbar.styles.css';
-import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, Avatar } from '@material-ui/core';
-
+import { Toolbar, Avatar, AppBar, Menu, Button, IconButton, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import { logo2 } from '@Assets';
 
 export class NavBar extends Component {
   render() {
     return (
-      <AppBar position="absolute">
-        <Toolbar>
-          <img alt="logo" src={logo2} className={styles.logo} />
-        </Toolbar>
-      </AppBar>
+      <div className={styles.root}>
+        <AppBar position="absolute">
+          <Toolbar>
+            <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <img alt="logo" src={logo2} className={styles.logo} />
+            <Typography variant="h6" color="inherit" className={styles.grow}>
+
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
